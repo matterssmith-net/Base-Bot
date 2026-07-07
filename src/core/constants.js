a0ad9189dea3ccca963dda6635d768b76270d915
🌸𝐌𝐢𝐤𝐮 ̶𝐂𝐡𝐚𝐧 ̶🩵✨ <apellidos05.nombre05@gmail.com>
2026-07-07 13:54:28 -0600

    Add files via upload

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,2 +1,6 @@
 export const EXIT_SUCCESS = 0;
 export const EXIT_FAILURE = 1;
+export const INVALID_CONFIG = 2;
+export const DATABASE_ERROR = 3;
+export const NETWORK_ERROR = 4;
+export const PERMISSION_DENIED = 5;
