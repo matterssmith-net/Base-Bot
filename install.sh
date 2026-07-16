72d00d0c6cc997d347882130a28909dcc12f2dee
🌸𝐌𝐢𝐤𝐮 ̶𝐂𝐡𝐚𝐧 ̶🩵✨ <apellidos05.nombre05@gmail.com>
2026-07-16 08:23:02 -0600

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -215,6 +215,8 @@ $line3
 sleep 15 && pkill mpv > /dev/null 2>&1 && sleep 2 && rm -f "/data/data/com.termux/files/usr/tmp/basebot_install.mp3" &
 echo -e "\033[01;32m\033[01mStarting Base Bot!!\n\033[0m"
 
+node scripts/migrate-runtime.js
+
 sync
 sleep 1
 
