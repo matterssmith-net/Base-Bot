27b43241671711fad8d76e7f2cca64c2a2332b40
🌸𝐌𝐢𝐤𝐮 ̶𝐂𝐡𝐚𝐧 ̶🩵✨ <apellidos05.nombre05@gmail.com>
2026-07-29 07:27:39 -0600

    Actualizar src.js

--- src.js
+++ src.js
@@ -1,3 +1,11 @@
+import { spawn } from "node:child_process";
+
 while (true) {
-  await import(`./install-javascript.js?t=${Date.now()}`);
+  await new Promise((resolve) => {
+    const child = spawn(process.execPath, ["./install-javascript.js"], {
+      stdio: "inherit",
+    });
+
+    child.on("exit", () => resolve());
+  });
 }
